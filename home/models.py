85eb02f52b3c57aed5f406558e79ca86b962e26b
me6edi <exited17@gmail.com>
2020-11-11 09:07:42 -0500

    model page code

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+# Create your models here
 
-# Create your models here.
+class Contact(models.Model):
+    sno = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    phone = models.IntegerField(max_length=12)
+    
