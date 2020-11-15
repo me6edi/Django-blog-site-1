65760048606b7f3d4b2a3d8e4b1fcc529189d27f
me6edi <exited17@gmail.com>
2020-11-15 10:45:36 -0500

    about page code

--- home/models.py
+++ home/models.py
@@ -4,5 +4,10 @@ from django.db import models
 class Contact(models.Model):
     sno = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    phone = models.IntegerField(max_length=12)
-    
+    phone = models.CharField(max_length=13)
+    email = models.CharField(max_length=100)
+    content = models.TextField()
+    timeStamp = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return 'Message from ' + self.name + ' - ' + self.email
--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse
+from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def home(request): 
@@ -7,8 +9,24 @@ def home(request):
 
 def contact(request):
     # return HttpResponse("This is contact")
+    if request.method=='POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        content = request.POST['content']
+        print(name, email, phone, content)
+
+        if len(name)<2 or len(email)<3 or len(phone)<10 or len(content)<4:
+            messages.error(request, "Please fill the form correctly")
+        else:
+
+            contact = Contact(name=name, email=email, phone=phone, content=content)
+            contact.save()
+            messages.success(request, "Your message has been successfully sent")
+
+
     return render(request, 'home/contact.html')
 
 def about(request): 
     # return HttpResponse('This is about')
-    return render(request, 'home/about.html')
+    return render(request, 'home/about.html') 
