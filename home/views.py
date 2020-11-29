2777a31d645abc1dc3682ee16300e389620fc001
me6edi <exited17@gmail.com>
2020-11-29 07:57:08 -0500

    base.html signup field complite

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from home.models import Contact
 from django.contrib import messages
+from django.contrib.auth.models import User
 from blog.models import Post
 
 # Create your views here.
@@ -47,3 +48,42 @@ def search(request):
 def about(request): 
     # return HttpResponse('This is about')
     return render(request, 'home/about.html') 
+
+def handleSignup(request):
+    if request.method == 'POST':
+        # Get the post parameters
+        username = request.POST['username']
+        fname= request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        # Checks for errorneous inputs
+        # username should be under 10 characters
+        if len(username) > 10:
+                    messages.error(request, "Username must be under 10 characters")
+                    return redirect('home')
+
+        # username should be alphamumeric
+        if not username.isalnum():
+                    messages.error(request, "Username should only contain letters and numbers")
+                    return redirect('home')
+
+
+        # passworeds should macth    
+        if pass1 != pass2:
+                    messages.error(request, "Passwords do not match")
+                    return redirect('home')
+
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.save()
+        messages.success(request, "Your account has been sucessfully created")
+        return redirect('home')
+
+    else:
+        return HttpResponse('404 - Not Found')
+
