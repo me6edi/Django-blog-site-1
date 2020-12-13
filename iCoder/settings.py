4ac080f83be3cbf7e8a6ee0caf6d857fde1f1c68
me6edi <exited17@gmail.com>
2020-12-13 10:09:05 -0500

    comment section add

--- iCoder/settings.py
+++ iCoder/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from django.contrib.messages import constants as messages
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,3 +126,7 @@ STATIC_URL = '/static/'
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,  "static"),
+]
